a845b5b98165696c1becb78c2c9583e26b5a4acc
Sasha <sasha.taylor7@gmail.com>
2024-10-06 20:42:08 -0600

    Feat: Add api helper function, delete Spotify api component files

--- src/api/SpotifySinglesApi.jsx
+++
@@ -1,7 +0,0 @@
-export async function getSpotifyAlbums() {
-  const playlistID = '';
-
-  return await fetch()
-  .then(data => data.json())
-  .then(list => list.items);
-}
--- src/api/YoutubeVidsApi.jsx
+++ src/api/YoutubeVidsApi.jsx
@@ -1,7 +1,5 @@
-export async function getYtVideos() {
-  const playlistID = '';
-
-  return await fetch()
+export async function getYtVideos(playlistID, apiKey) {
+  return await fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistID}&key=${apiKey}`)
   .then(data => data.json())
   .then(list => list.items);
 }
