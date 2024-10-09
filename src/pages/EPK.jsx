f0b391df6f0701b9b266735e2854d53c7b24d36f
Sasha <sasha.taylor7@gmail.com>
2024-10-08 22:13:00 -0600

    Feat: EPK bio styling mvp ready

--- src/pages/EPK.jsx
+++ src/pages/EPK.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import AltHeader from '../components/AltHeader.jsx'
+import otoLogo from '../assets/otoLogo.png'
 import otoClick from '../assets/epk/otoClick.png'
 import tonesPurp from '../assets/epk/tonesPurp.png'
 import moonbeams from '../assets/epk/moonbeams.png'
@@ -17,24 +18,30 @@ const EPK = () => {
       <AltHeader/>
       <section>
         <h1 className='text-7xl text-amber-400 text-center'>EPK</h1>
-        {Viewport().width < 640 ? <div className='text-xl flex flex-col justify-items-center mx-[10px] gap-2'>
+        {Viewport().width < 640 ? 
+        <div className='text-xl flex flex-col items-center mx-[10px] gap-2'>
           <div className='flex flex-col gap-2'>
             <img className='h-[200px] w-[1000px] object-cover object-[center_23%]' src={tonesPurp} alt='OTO Big Tones Performing' />
           </div>
-        <p className='flex items-center text-slate-200'>Anthony Pasqua aka Big Tones is an Indigenous Recording Artist born in Surrey, BC. He grew up in Regina, SK and has heavy ancestral ties to Treaty 4 Territory coming from Pasqua First Nation #79 & George Gordon First Nation #84. Breaking the cycle of intergenerational trauma, Big Tones uses music as an outlet, finding therapeutic release in storytelling through songwriting.</p>
-            <img className='h-[200px] w-[1000px] object-cover object-[center_43%]'  src={otoClick} alt='OTO Group' />
-        <p className='flex items-center text-slate-200'>Big Tones is a versatile artist, captivating performer and artist mentor. With a focus on old school hip hop sound, his powerful and soothing voice with intricate flows keeps listeners intrigued lyric after lyric. Big Tones shares his personal experience and offers perspectives on life through the eyes of Indigenous peoples. He moves audiences with his strong stage presence and masterful command of the mic. Big Tones uses hip hop as a vessel to communicate aspects of pain and trauma, moving through to healing, celebration and taking back pride through culture. Recently getting airtime on CBC Radio Active with his new single Creased AF1's Big Tones is striving with a hunger for more. Big Tones has worked as an artist mentor with the Community Arts Mentorship Program, Paved Arts, Creative City Centre Regina and at Kilburn Hall. He appreciates the opportunity to encourage youth voices.</p>
-          <br></br>
+        <p className='text-slate-200'>Anthony Pasqua aka Big Tones is an Indigenous Recording Artist born in Surrey, BC. He grew up in Regina, SK and has heavy ancestral ties to Treaty 4 Territory coming from Pasqua First Nation #79 & George Gordon First Nation #84. Breaking the cycle of intergenerational trauma, Big Tones uses music as an outlet, finding therapeutic release in storytelling through songwriting.</p>
+          <img className='h-[200px] w-[1000px] object-cover object-[center_43%]'  src={otoClick} alt='OTO Group' />
+          <p className='text-sm text-amber-400 translate-y-[-30px] translate-x-[-150px] mb-[-30px]'>OTO Family</p>            
+        <p className='text-slate-200'>Big Tones is a versatile artist, captivating performer and artist mentor. With a focus on old school hip hop sound, his powerful and soothing voice with intricate flows keeps listeners intrigued lyric after lyric. Big Tones shares his personal experience and offers perspectives on life through the eyes of Indigenous peoples. He moves audiences with his strong stage presence and masterful command of the mic. Big Tones uses hip hop as a vessel to communicate aspects of pain and trauma, moving through to healing, celebration and taking back pride through culture. Recently getting airtime on CBC Radio Active with his new single Creased AF1's Big Tones is striving with a hunger for more. Big Tones has worked as an artist mentor with the Community Arts Mentorship Program, Paved Arts, Creative City Centre Regina and at Kilburn Hall. He appreciates the opportunity to encourage youth voices.</p>
+        <img className='h-[75px] w-[80px] mb-[5px]' src={otoLogo} alt='Oto Logo'/>
+
         </div>
-        : Viewport().width < 768 ?
-        <div className='text-xl flex flex-col items-center justify-items-center sm:mx-[10px] md:mx-[50px] lg:mx-[150px] gap-2 md:gap-4'>
-          <div className='flex flex-row gap-2'>
-            <img className='size-80' src={tonesPurp} alt='OTO Big Tones Performing' />
-        <p className='flex items-center text-slate-200'>Anthony Pasqua aka Big Tones is an Indigenous Recording Artist born in Surrey, BC. He grew up in Regina, SK and has heavy ancestral ties to Treaty 4 Territory coming from Pasqua First Nation #79 & George Gordon First Nation #84. Breaking the cycle of intergenerational trauma, Big Tones uses music as an outlet, finding therapeutic release in storytelling through songwriting. Big Tones is a versatile artist, captivating performer and artist mentor. With a focus on old school hip hop sound, his powerful and soothing voice with intricate flows keeps listeners intrigued lyric after lyric. Big Tones shares his personal experience and offers perspectives on life through the eyes of Indigenous peoples. He moves audiences with his strong stage presence and masterful command of the mic. Big Tones uses hip hop as a vessel to communicate aspects of pain and trauma, moving through to healing, celebration and taking back pride through culture. Recently getting airtime on CBC Radio Active with his new single Creased AF1's Big Tones is striving with a hunger for more. Big Tones has worked as an artist mentor with the Community Arts Mentorship Program, Paved Arts, Creative City Centre Regina and at Kilburn Hall. He appreciates the opportunity to encourage youth voices.</p>
+        : 
+        <div className='text-xl flex flex-row sm:mx-[10px] md:mx-[50px] lg:mx-[50px] gap-2 md:gap-4'>
+          <div className='h-[300px] w-[1800px] flex flex-col items-center'>
+          <img className='h-[250px] w-[260px] object-cover'src={tonesPurp} alt='OTO Big Tones Performing' />
+          <img className='h-[75px] md:w-[80px] lg:h-[150px] lg:w-[160px] object-cover' src={otoLogo} alt='Oto Logo'/>
           </div>
-            <img className='h-[250px] w-[500px] object-cover ' src={otoClick} alt='OTO Group' />
-            <br></br>
-        </div> : 
+        <div className='flex flex-col items-center gap-2'>
+      <p className='text-slate-200'>Anthony Pasqua aka Big Tones is an Indigenous Recording Artist born in Surrey, BC. He grew up in Regina, SK and has heavy ancestral ties to Treaty 4 Territory coming from Pasqua First Nation #79 & George Gordon First Nation #84. Breaking the cycle of intergenerational trauma, Big Tones uses music as an outlet, finding therapeutic release in storytelling through songwriting. Big Tones is a versatile artist, captivating performer and artist mentor. With a focus on old school hip hop sound, his powerful and soothing voice with intricate flows keeps listeners intrigued lyric after lyric. Big Tones shares his personal experience and offers perspectives on life through the eyes of Indigenous peoples. He moves audiences with his strong stage presence and masterful command of the mic. Big Tones uses hip hop as a vessel to communicate aspects of pain and trauma, moving through to healing, celebration and taking back pride through culture. Recently getting airtime on CBC Radio Active with his new single Creased AF1's Big Tones is striving with a hunger for more. Big Tones has worked as an artist mentor with the Community Arts Mentorship Program, Paved Arts, Creative City Centre Regina and at Kilburn Hall. He appreciates the opportunity to encourage youth voices.</p>
+      <img className='h-[250px] w-[800px] object-cover ' src={otoClick} alt='OTO Group' />
+        </div>
+          <br></br>
+      </div> 
         }
         
       </section>
@@ -52,11 +59,11 @@ const EPK = () => {
         <h3 className='text-slate-200'>Live Mixtape, Saskatoon, SK</h3>
       </section>
       <hr></hr>
-      <section className='pressLinks'>
+      {/* <section className='pressLinks'>
         <h1 className='altHeader'>
           PRESS
         </h1>
-      </section>
+      </section> */}
     </div>
   );
 };
