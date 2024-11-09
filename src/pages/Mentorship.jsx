1f2a1a06a8f32475a44244da3732a9ebf63c0d01
Sasha <sasha.taylor7@gmail.com>
2024-11-08 22:13:47 -0700

    Fix: Update mentorship video styli g

--- src/pages/Mentorship.jsx
+++ src/pages/Mentorship.jsx
@@ -30,7 +30,7 @@ const Mentorship = () => {
       <section>
         <h1 className='text-amber-400 text-center text-8xl'>Mentorship</h1>
         <div className='flex flex-col justify-center'>
-        <p className='text-justify my-4 sm:mx-4 md:mx-10 lg:mx-[15vw] text-xl text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt praesent semper feugiat nibh sed pulvinar. Consequat semper viverra nam libero. Sapien pellentesque habitant morbi tristique senectus et netus et. Amet facilisis magna etiam tempor orci eu lobortis elementum. Arcu ac tortor dignissim convallis aenean. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum. Lobortis elementum nibh tellus molestie nunc non. Mi tempus imperdiet nulla malesuada pellentesque elit. Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit. Dictum at tempor commodo ullamcorper a lacus vestibulum. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna.
+        <p className='text-justify my-4 mx-4 md:mx-10 lg:mx-[15vw] text-xl text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt praesent semper feugiat nibh sed pulvinar. Consequat semper viverra nam libero. Sapien pellentesque habitant morbi tristique senectus et netus et. Amet facilisis magna etiam tempor orci eu lobortis elementum. Arcu ac tortor dignissim convallis aenean. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum. Lobortis elementum nibh tellus molestie nunc non. Mi tempus imperdiet nulla malesuada pellentesque elit. Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit. Dictum at tempor commodo ullamcorper a lacus vestibulum. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna.
         Rhoncus urna neque viverra justo nec ultrices. Risus in hendrerit gravida rutrum. Elementum sagittis vitae et leo. Eu facilisis sed odio morbi quis commodo odio. Egestas sed tempus urna et pharetra. Nisl pretium fusce id velit ut tortor. Eget velit aliquet sagittis id. Lorem ipsum dolor sit amet. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget. Integer enim neque volutpat ac tincidunt vitae semper. Justo nec ultrices dui sapien eget. Sed elementum tempus egestas sed sed risus pretium quam. Dictum at tempor commodo ullamcorper a lacus vestibulum. Tempor nec feugiat nisl pretium fusce id velit ut.</p>
 
         </div>
@@ -57,7 +57,7 @@ const Mentorship = () => {
             mentorshipItems.map((item) => 
               <>
               <SwiperSlide>
-              <div className='flex flex-col items-center justify-center mx-16 my-2'>
+              <div className='flex flex-col items-center justify-center my-2'>
                 <img className='sm:h-[225px] sm:w-[1000px] md:h-96 md:w-[70vw] object-cover object-[center_43%]' src={item.image} alt={item.caption} />
                 <h1 className='text-xl text-amber-400 w-[90vw]'>{item.caption}</h1>
                 <br></br>
@@ -100,11 +100,12 @@ const Mentorship = () => {
         }
       </Swiper>
           
+          <hr className='h-0.5 rounded-md border-0 w-10/12 bg-amber-400 outline-amber-400 my-4'></hr>
           </div>
         
       </section>
       <section>
-      <div className='mx-16 my-2'>
+      <div className='flex flex-col items-center mx-16 my-2'>
             <VideoModalBlock playlistId={youtubePlaylists.mentorship} apiKey={ytApiKey}/>
           </div>
       </section>
