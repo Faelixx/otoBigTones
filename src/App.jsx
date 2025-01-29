1cfc0e131e52ad8e9df8f86c0e9362e6d2649f7a
faelixx1337 <sasha.taylor7@gmail.com>
2025-01-29 00:45:09 -0700

    feat code split react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import React from 'react';
+
 import {HashRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import EPK from './pages/EPK';
-import Mentorship from './pages/Mentorship';
-import Music from './pages/Music';
-import Tour from './pages/Tour';
-import Videos from './pages/Videos';
+// import Home from './pages/Home';
+// import EPK from './pages/EPK';
+// import Mentorship from './pages/Mentorship';
+// import Contact from './pages/Contact';
+// import Music from './pages/Music';
+// import Tour from './pages/Tour';
+// import Videos from './pages/Videos';
 import Footer from './components/Footer';
 
 import './output.css';
@@ -14,6 +16,13 @@ import Navbar from './components/Navbar';
 import MenuButton from './components/MenuButton';
 import Viewport from './helpers/Viewport';
 
+const LazyHome = React.lazy(() => import('./pages/Home'));
+const LazyEPK = React.lazy(() => import('./pages/EPK'));
+const LazyContact = React.lazy(() => import('./pages/Contact'));
+const LazyMentorship = React.lazy(() => import('./pages/Mentorship'));
+const LazyMusic = React.lazy(() => import('./pages/Music'));
+const LazyTour = React.lazy(() => import('./pages/Tour'));
+const LazyVideos = React.lazy(() => import('./pages/Videos'));
 
 function App() {
 
@@ -26,13 +35,41 @@ function App() {
         <MenuButton/>
       }
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/contact' element={<Contact />}/>
-          <Route path='/epk' element={<EPK />}/>
-          <Route path='/mentorship' element={<Mentorship />}/>
-          <Route path='/music' element={<Music />}/>
-          <Route path='/tour' element={<Tour />}/>
-          <Route path='/videos' element={<Videos />}/>
+          <Route path='/' element={
+            <React.Suspense fallback='loading...'>
+              <LazyHome />
+            </React.Suspense>
+          }/>
+          <Route path='/contact' element={
+            <React.Suspense fallback='loading...'>
+            <LazyContact />
+            </React.Suspense>
+          }/>            
+          <Route path='/epk' element={
+            <React.Suspense fallback='loading...'>
+            <LazyEPK />
+            </React.Suspense>
+          }/>
+          <Route path='/mentorship' element={
+            <React.Suspense fallback='loading...'>
+            <LazyMentorship />
+            </React.Suspense>
+        }/>
+          <Route path='/music' element={
+            <React.Suspense fallback='loading...'>
+            <LazyMusic />
+            </React.Suspense>
+        }/>
+          <Route path='/tour' element={
+            <React.Suspense fallback='loading...'>
+            <LazyTour />
+            </React.Suspense>
+        }/>
+          <Route path='/videos' element={
+            <React.Suspense fallback='loading...'>
+            <LazyVideos />
+            </React.Suspense>
+        }/>
         </Routes>
       </HashRouter>
       <Footer />
